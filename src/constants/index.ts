5f6e301a74317ac1a14269d52a0ba3b215abfd5a
mdkamranalam <work.mdkamranalam@gmail.com>
2026-01-31 19:40:43 +0530

    implement data provider

---
+++ src/constants/index.ts
@@ -0,0 +1,13 @@
+export const DEPARTMENT = [
+  "CS",
+  "Math",
+  "Physics",
+  "Chemistry",
+  "Biology",
+  "English",
+];
+
+export const DEPARTMENT_OPTIONS = DEPARTMENT.map((dept) => ({
+  value: dept,
+  label: dept,
+}));
