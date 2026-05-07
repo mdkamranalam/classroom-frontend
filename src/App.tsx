65890813fac5369c97d4353bd3ec915ca8970528
mdkamranalam <work.mdkamranalam@gmail.com>
2026-05-08 00:06:10 +0530

    create class form

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,89 @@
-import { Refine } from "@refinedev/core";
-import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
-import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+import {Refine} from "@refinedev/core";
+import {DevtoolsPanel, DevtoolsProvider} from "@refinedev/devtools";
+import {RefineKbar, RefineKbarProvider} from "@refinedev/kbar";
 
-import routerProvider, {
-  DocumentTitleHandler,
-  UnsavedChangesNotifier,
-} from "@refinedev/react-router";
-import { BrowserRouter, Outlet, Route, Routes } from "react-router";
+import routerProvider, {DocumentTitleHandler, UnsavedChangesNotifier,} from "@refinedev/react-router";
+import {BrowserRouter, Outlet, Route, Routes} from "react-router";
 import "./App.css";
-import { Toaster } from "./components/refine-ui/notification/toaster";
-import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
-import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
+import {Toaster} from "./components/refine-ui/notification/toaster";
+import {useNotificationProvider} from "./components/refine-ui/notification/use-notification-provider";
+import {ThemeProvider} from "./components/refine-ui/theme/theme-provider";
 import Dashboard from "./pages/Dashboard";
-import { BookOpen, Home } from "lucide-react";
-import { Layout } from "./components/refine-ui/layout/layout";
+import {BookOpen, GraduationCap, Home} from "lucide-react";
+import {Layout} from "./components/refine-ui/layout/layout";
 import SubjectsList from "./pages/subjects/List";
 import SubjectsCreate from "./pages/subjects/Create";
+import ClassesList from "./pages/classes/List";
+import ClassesCreate from "./pages/classes/Create";
 import {dataProvider} from "@/providers/data.ts";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <RefineKbarProvider>
-        <ThemeProvider>
-          <DevtoolsProvider>
-            <Refine
-              dataProvider={dataProvider}
-              notificationProvider={useNotificationProvider()}
-              routerProvider={routerProvider}
-              options={{
-                syncWithLocation: true,
-                warnWhenUnsavedChanges: true,
-                projectId: "th4BMy-GIhfCZ-iLZYlm",
-              }}
-              resources={[
-                {
-                  name: "dashboard",
-                  list: "/",
-                  meta: { label: "Home", icon: <Home /> },
-                },
-                {
-                  name: "subjects",
-                  list: "/subjects",
-                  create: "/subjects/create",
-                  meta: { label: "Subjects", icon: <BookOpen /> },
-                },
-              ]}
-            >
-              <Routes>
-                <Route
-                  element={
-                    <Layout>
-                      <Outlet />
-                    </Layout>
-                  }
-                >
-                  <Route path="/" element={<Dashboard />} />
+    return (
+        <BrowserRouter>
+            <RefineKbarProvider>
+                <ThemeProvider>
+                    <DevtoolsProvider>
+                        <Refine
+                            dataProvider={dataProvider}
+                            notificationProvider={useNotificationProvider()}
+                            routerProvider={routerProvider}
+                            options={{
+                                syncWithLocation: true,
+                                warnWhenUnsavedChanges: true,
+                                projectId: "th4BMy-GIhfCZ-iLZYlm",
+                            }}
+                            resources={[
+                                {
+                                    name: "dashboard",
+                                    list: "/",
+                                    meta: {label: "Home", icon: <Home/>},
+                                },
+                                {
+                                    name: "subjects",
+                                    list: "/subjects",
+                                    create: "/subjects/create",
+                                    meta: {label: "Subjects", icon: <BookOpen/>},
+                                },
+                                {
+                                    name: "classes",
+                                    list: "/classes",
+                                    create: "/classes/create",
+                                    meta: {label: "Classes", icon: <GraduationCap/>},
+                                },
+                            ]}
+                        >
+                            <Routes>
+                                <Route
+                                    element={
+                                        <Layout>
+                                            <Outlet/>
+                                        </Layout>
+                                    }
+                                >
+                                    <Route path="/" element={<Dashboard/>}/>
 
-                  <Route path="subjects">
-                    <Route index element={<SubjectsList />} />
-                    <Route path="create" element={<SubjectsCreate />} />
-                  </Route>
-                </Route>
-              </Routes>
-              <Toaster />
-              <RefineKbar />
-              <UnsavedChangesNotifier />
-              <DocumentTitleHandler />
-            </Refine>
-            <DevtoolsPanel />
-          </DevtoolsProvider>
-        </ThemeProvider>
-      </RefineKbarProvider>
-    </BrowserRouter>
-  );
+                                    <Route path="subjects">
+                                        <Route index element={<SubjectsList/>}/>
+                                        <Route path="create" element={<SubjectsCreate/>}/>
+                                    </Route>
+
+                                    <Route path="classes">
+                                        <Route index element={<ClassesList/>}/>
+                                        <Route path="create" element={<ClassesCreate/>}/>
+                                    </Route>
+                                </Route>
+                            </Routes>
+                            <Toaster/>
+                            <RefineKbar/>
+                            <UnsavedChangesNotifier/>
+                            <DocumentTitleHandler/>
+                        </Refine>
+                        <DevtoolsPanel/>
+                    </DevtoolsProvider>
+                </ThemeProvider>
+            </RefineKbarProvider>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/components/upload-widget.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const UploadWidget = () => {
+    return (
+        <div>UploadWidget</div>
+    )
+}
+export default UploadWidget
